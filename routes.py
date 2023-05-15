d5b381ac9e5a5930b3c9a952d690f8f22d8c5f53
BURNSIDE HIGH SCHOOL <code@burnside.school.nz>
2023-05-15 12:56:06 +1200

    add static(css+images), add more stuff in tempate

--- routes.py
+++ routes.py
@@ -1,11 +1,21 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello():
-    return 'Hello, World!'
+    return render_template("home.html")
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
