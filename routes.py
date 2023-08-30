584332ea1363bc0479f1cd7c14ee8c75cc51cb9a
Jamesnef <22629@burnside.school.nz>
2023-08-30 13:59:25 +1200

    update routes.py
    update form

--- routes.py
+++ routes.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 import sqlite3
 
 app = Flask(__name__)
@@ -10,11 +10,55 @@ def hello():
 
 @app.route("/contact")
 def contact():
-    return render_template("contact.html")
+    return render_template('contact.html')
+
+    
 
 @app.route("/about")
 def about():
     return render_template("about.html")
+
+@app.route("/all_character")
+def all_character():
+    conn=sqlite3.connect("starwar.db")
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM Character")
+    results = cur.fetchall()
+    print(results)
+    return render_template("all_character.html", results=results)
+
+
+@app.route("/Character/<int:id>")
+def Character(id):
+    conn=sqlite3.connect("starwar.db")
+    cur=conn.cursor()
+    cur.execute("SELECT * FROM Character WHERE id=?",(id,))
+    Character = cur.fetchone()
+    print(Character)
+    cur.execute("SELECT * FROM Abilities WHERE id=?",(id,))
+    Abilities = cur.fetchone()
+    print(Abilities)
+    cur.execute("SELECT name FROM The_side WHERE id in( SELECT aid FROM Character_abilities WHERE cid=?)",(id,))
+    The_side = cur.fetchall()
+    print(The_side)
+    
+    return render_template("Character.html",Character=Character, Abilities=Abilities, The_side=The_side )
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        firstname = request.form['firstname']
+        lastname = request.form['lastname']
+        subject = request.form['subject']
+        
+        conn = sqlite3.connect('starwar.db')
+        cursor = conn.cursor()
+        cursor.execute("INSERT INTO entries (firstname, lastname, subject) VALUES (?, ?, ?)", (firstname, lastname, subject))
+        conn.commit()
+        conn.close()
+    
+    return render_template('contact.html')
+
 
 
 
