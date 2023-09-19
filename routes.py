371f41a12666c89084b086baa9d7f61b477a9707
Jamesnef <22629@burnside.school.nz>
2023-09-19 14:09:46 +1200

    pep8 / change background + nav

--- routes.py
+++ routes.py
@@ -3,21 +3,17 @@ import sqlite3
 
 app = Flask(__name__)
 
-
 @app.route('/')
-def hello():
-    return render_template("home.html")
-
+def home():
+    return render_template("home.html")  # HTML template named 'home.html' is created
 
 @app.route("/contact")
 def contact():
-    return render_template('contact.html')
-
+    return render_template('contact.html')  # HTML template named 'contact.html' is created
 
 @app.route("/about")
 def about():
-    return render_template("about.html")
-
+    return render_template("about.html")  # HTML template named 'about.html' is created
 
 @app.route("/all_character")
 def all_character():
@@ -28,38 +24,37 @@ def all_character():
     print(results)
     return render_template("all_character.html", results=results)
 
-
 @app.route("/Character/<int:id>")
 def Character(id):
     conn = sqlite3.connect("starwar.db")
     cur = conn.cursor()
-    cur.execute("SELECT * FROM Character WHERE id=?", (id,))
+    cur.execute("SELECT * FROM Character WHERE id=?", (id,))  # Execute SQLite query to retrieve data from "Character" table in the database.
     Character = cur.fetchone()
     print(Character)
-    cur.execute("SELECT * FROM Abilities WHERE id=?", (id,))
+    cur.execute("SELECT * FROM Abilities WHERE id=?", (id,))  # Execute SQLite query to retrieve data from "Abilities" table in the database.
     Abilities = cur.fetchone()
     print(Abilities)
-    cur.execute("SELECT name FROM The_side WHERE id in( SELECT aid FROM Character_abilities WHERE cid=?)", (id,))
+    cur.execute("SELECT name FROM The_side WHERE id IN (SELECT aid FROM Character_abilities WHERE cid=?)", (id,))  # Retrieves the names of the sides (e.g., light side or dark side) associated with the character.
     The_side = cur.fetchall()
     print(The_side)
 
     return render_template("Character.html", Character=Character, Abilities=Abilities, The_side=The_side)
 
-
 @app.route('/contact', methods=['GET', 'POST'])
-def index():
-    if request.method == 'POST':
+def index():  # Function named index to handle the '/contact' route.
+    if request.method == 'POST':  # POST is used when submitting data from the server.
         firstname = request.form.get('firstname')
         lastname = request.form.get('lastname')
         subject = request.form.get('subject')
-        conn = sqlite3.connect('starwar.db')
+        # First/last/subject + name = id where user input data submitted from POST.
+        conn = sqlite3.connect('starwar.db')  # Route connects to SQLite (starwar.db).
         cursor = conn.cursor()
         cursor.execute("INSERT INTO entries (firstname, lastname, subject) VALUES (?, ?, ?)", (firstname, lastname, subject))
+        # Execute an SQLite query into the table firstname, lastname, and subject.
         conn.commit()
         conn.close()
         return redirect('/')
     return render_template('contact.html')
-
 
 if __name__ == "__main__":
     app.run(debug=True)
